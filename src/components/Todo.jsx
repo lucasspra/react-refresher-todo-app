31fa5ca9b7771a1fc593e15a1db747294a75eb94
Lucas Souza <lucas.souza@ifood.com.br>
2021-06-30 10:18:34 -0300

    Set closeModalHandler and deleteHandler

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -8,6 +8,10 @@ function Todo(props) {
     setModalIsOpen(true);
   }
 
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
@@ -16,8 +20,10 @@ function Todo(props) {
           Delete
         </button>
       </div>
-      {modalIsOpen && <Modal />}
-      {modalIsOpen && <Backdrop />}
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
     </div>
   );
 }
